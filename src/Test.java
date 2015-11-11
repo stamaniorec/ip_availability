cd13e0fcefdee077676f7532c914843300e8f6b1
stamaniorec <stamaniorec@gmail.com>
2015-11-11 11:01:28 +0200

    I/O from socket instead of std i/o

--- src/Test.java
+++ src/Test.java
@@ -1,14 +1,10 @@
-import java.io.IOException;
-import java.io.PrintStream;
-import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
 
-public class Main {
+public class Test {
 
 	private static Set<String> currentlyLoggedUsers = new HashSet<String>();
 	private static HashMap<String, Integer> usersToLoginCount = new HashMap<String, Integer>();
@@ -69,39 +65,36 @@ public class Main {
 		}
 	}
 	
-	public static void main(String[] args) throws UnknownHostException, IOException {
-		Socket s = new Socket("127.0.0.1", 31111);
-		final PrintStream out = new PrintStream(s.getOutputStream());
-		Scanner input = new Scanner(s.getInputStream());
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 		while(true) {
-			out.println("въведете команда: ");
+			System.out.println("въведете команда: ");
 			String line = input.nextLine();
 
 			String[] arr = line.split(":");
 			if(arr.length >= 2) {
 				if("login".equals(arr[1])) {
-					out.println(login(arr));
+					System.out.println(login(arr));
 				} else if("logout".equals(arr[1])) {
-					out.println(logout(arr));
+					System.out.println(logout(arr));
 				} else if("info".equals(arr[1])) {
-					out.println(info(arr));
+					System.out.println(info(arr));
 				} else if("listavailable".equals(arr[1])) {
-					out.println(listavailable(arr));
+					System.out.println(listavailable(arr));
 				} else if("shutdown".equals(arr[1])) {
 					String output = shutdown(arr);
 					if("ok".equals(output)) {
 						break;
 					} else {
-						out.println(output);
+						System.out.println(output);
 					}
 				} else {
-					out.println("error:unknowncommand");
+					System.out.println("error:unknowncommand");
 				}
 			} else {
-				out.println("error:unknowncommand");
+				System.out.println("error:unknowncommand");
 			}
 		}
 		input.close();
-		s.close();
 	}	
 }
