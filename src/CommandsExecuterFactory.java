069f75026024264f62361562da0a442534706e5e
stamaniorec <stamaniorec@gmail.com>
2015-11-13 18:13:24 +0200

    Refactored CommandsExecuterFactory

--- src/CommandsExecuterFactory.java
+++ src/CommandsExecuterFactory.java
@@ -1,31 +1,21 @@
 class CommandsExecuterFactory {
-//	private UserClient clientSession;
-
-//	public CommandsExecuterFactory(UserClient clientSession) {
-//		this.clientSession = clientSession;
-//	}
-
-	public static Command getCommand(String line, UserClient clientSession) {
-		String[] arr = line.split(":");
-		if (arr.length > 2)
-			return null;
-		if (arr.length == 1) {
-			if ("shutdown".equals(arr[0])) {
-				return new ShutdownCommand(clientSession);
-			} else if ("listavailable".equals(arr[0])) {
-				return new ListAvailableCommand(clientSession);
-			} else if ("listabsent".equals(arr[0])) {
-				return new ListAbsentCommand(clientSession);
-			} else if ("logout".equals(arr[0])) {
-				return new LogoutCommand(clientSession);
-			}
-		} else if (arr.length == 2) {
-			if ("login".equals(arr[0])) {
-				return new LoginCommand(clientSession);
-			} else if ("info".equals(arr[0])) {
-				return new InfoCommand(clientSession);
-			}
+	
+	public static Command getCommand(String inputLine, UserClient clientSession) {
+		String commandName = inputLine.split(":")[0];
+		if ("shutdown".equals(commandName)) {
+			return new ShutdownCommand(clientSession);
+		} else if ("listavailable".equals(commandName)) {
+			return new ListAvailableCommand(clientSession);
+		} else if ("listabsent".equals(commandName)) {
+			return new ListAbsentCommand(clientSession);
+		} else if ("logout".equals(commandName)) {
+			return new LogoutCommand(clientSession);
+		} else if ("login".equals(commandName)) {
+			return new LoginCommand(clientSession);
+		} else if ("info".equals(commandName)) {
+			return new InfoCommand(clientSession);
 		}
 		return null;
 	}
+	
 }
