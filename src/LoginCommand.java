3ed3e5afd1d2d1aae332c5319f04d9fefd7a9ecb
stamaniorec <stamaniorec@gmail.com>
2015-11-14 11:00:07 +0200

    Further refactoring of LoginCommand

--- src/LoginCommand.java
+++ src/LoginCommand.java
@@ -1,39 +1,43 @@
 
 class LoginCommand extends Command {
-	Server server;
-	User currentSessionUser;
+	private Server server;
+	private User sessionUser;
 	
 	public LoginCommand(UserClient clientSession) {
 		super(clientSession);
 		server = clientSession.getServer();
-		currentSessionUser = clientSession.getUser();
+		sessionUser = clientSession.getUser();
 	}
 	
 	@Override
 	public String execute(String[] args) {
 		String username = args[1];
+		
 		User userToLogin = server.getUser(username);
 		if(userToLogin == null) {
 			userToLogin = new User(username);
 			server.getUsers().put(username, userToLogin);
-		} else {
-			logoutFromOtherSessions(userToLogin.getUsername());			
 		}
-		if(userToLogin != currentSessionUser) {
-			new LogoutCommand(clientSession).execute(new String[0]);
+
+		if(userToLogin != sessionUser) {
+			logoutFromOtherSessions(userToLogin.getUsername());
+			logoutSessionUser(clientSession);
 			userToLogin.login();
+			clientSession.setUser(userToLogin);
 		}
-		clientSession.setUser(userToLogin);
+		
 		return "ok";
 	}
 	
+	private void logoutSessionUser(UserClient session) {
+		new LogoutCommand(session).execute(new String[0]);
+	}
+	
 	private void logoutFromOtherSessions(String username) {
-		for(UserClient session : server.getClients()) {
-			if(session != clientSession) {
-				User user = session.getUser();
-				if(user != null && user.getUsername().equals(username)) {
-					new LogoutCommand(session).execute(new String[0]);
-				}
+		for (UserClient session : server.getClients()) {
+			User sessionUser = session.getUser();
+			if (sessionUser != null && sessionUser.getUsername().equals(username)) {
+				logoutSessionUser(session);
 			}
 		}
 	}
